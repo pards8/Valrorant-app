97a9c58356c6a36a4b34d4adc495814488fed937
pards8 <pardojuan811@gmail.com>
2025-06-19 10:20:52 +0800

    day 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,41 +2,121 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold (
+    home: Scaffold(
       appBar: AppBar(
-        title: Text ('Valorant App' , style: TextStyle(
+        title: Text(
+          'Valorant App',
+          style: TextStyle(
             fontSize: 30,
             fontWeight: FontWeight.bold,
             color: Colors.white,
             shadows: [
-            Shadow(
-            blurRadius:10.0,
-            color: Colors.black,
-            offset: Offset(2.0,2.0) ),
-]
-        ),),
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.black,
+                offset: Offset(2.0, 2.0),
+              ),
+            ],
+          ),
+        ),
         backgroundColor: Colors.redAccent[400],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('John Adrian S. Pardo', style: TextStyle(
-                color: Colors.redAccent,
-                fontSize: 20
+      body: Column(
+        children: [
+          // Name Row
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                 Text(
+                    'Name:',
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 20,
+                    ),
+                  ),
+
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  color: Colors.deepOrange,// Padding inside the text widget
+                  child: Text(
+                    'John Adriann S. Pardo',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
             ),
+          ),
+
+          // Age Row
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+
+                 Text(
+                    'Age:',
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 20,
+
+                    ),
+                  ),
+
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  color: Colors.amberAccent,// Padding inside the text widget
+                  child: Text(
+                    '21 Years Old',
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text('Map Line Ups', style: TextStyle(
-                color: Colors.redAccent,
-                fontSize: 20),
+          ),
+
+          // Gender Row
+          Container(
+            margin: EdgeInsets.fromLTRB(5, 4, 3, 2),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              // Padding inside the text widget
+              Text(
+                    'Gender:',
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 20,
+                    ),
+                  ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  color: Colors.lightGreen,// Padding inside the text widget
+                  child: Text(
+                    'Male',
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text('Agent Drafts', style: TextStyle(color:
-                Colors.redAccent,
-                fontSize: 20),
-            ),
-          ],
-        ),
+          ),
+        ],
+      ),
     ),
-  )));
+  ));
 }
