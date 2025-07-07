4046259bab0e82ca0fcd2616eae59643ff23524b
pards8 <pardojuan811@gmail.com>
2025-07-07 11:18:46 +0800

    net ninja tuts

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hello/WorldTIme/pages/choose_location.dart';
+import 'package:hello/WorldTIme/pages/loading.dart';
 import 'package:hello/pages/AddAgent.dart';
 import 'package:hello/pages/Dashboard.dart';
 import 'package:hello/pages/ListItems.dart';
+import 'package:hello/WorldTIme/pages/home.dart';
 
 void main() {
   runApp(MaterialApp(
+    initialRoute: '/home',
     routes: {
-      '/': (context) => Listitems(),
-      '/add' : (context) => AddAgent()
+      '/': (context) => Loading(),
+      '/home' : (context) => Home(),
+      '/location': (context) => ChooseLocation(),
     },
 
   ));
